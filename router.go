43a8ee9baf43c37db79623d5a1c1a8829cceb57c
Daniel Imfeld <daniel@danielimfeld.com>
2015-06-09 15:29:05 -0500

    Improve godoc content.
    Fixes #11

--- router.go
+++ router.go
@@ -1,3 +1,7 @@
+// This is inspired by Julien Schmidt's httprouter, in that it uses a patricia tree, but the
+// implementation is rather different. Specifically, the routing rules are relaxed so that a
+// single path segment may be a wildcard in one route and a static token in another. This gives a
+// nice combination of high performance with a lot of convenience in designing the routing patterns.
 package httptreemux
 
 import (
@@ -6,6 +10,7 @@ import (
 	"net/http"
 )
 
+// The params argument contains the parameters parsed from wildcards and catch-alls in the URL.
 type HandlerFunc func(http.ResponseWriter, *http.Request, map[string]string)
 type PanicHandler func(http.ResponseWriter, *http.Request, interface{})
 
@@ -83,7 +88,69 @@ func (t *TreeMux) Dump() string {
 	return t.root.dumpTree("", "")
 }
 
-func (t *TreeMux) Handle(verb, path string, handler HandlerFunc) {
+// Path elements starting with : indicate a wildcard in the path. A wildcard will only match on a
+// single path segment. That is, the pattern `/post/:postid` will match on `/post/1` or `/post/1/`,
+// but not `/post/1/2`.
+//
+// A path element starting with * is a catch-all, whose value will be a string containing all text
+// in the URL matched by the wildcards. For example, with a pattern of `/images/*path` and a
+// requested URL `images/abc/def`, path would contain `abc/def`.
+//
+// # Routing Rule Priority
+//
+// The priority rules in the router are simple.
+//
+// 1. Static path segments take the highest priority. If a segment and its subtree are able to match the URL, that match is returned.
+//
+// 2. Wildcards take second priority. For a particular wildcard to match, that wildcard and its subtree must match the URL.
+//
+// 3. Finally, a catch-all rule will match when the earlier path segments have matched, and none of the static or wildcard conditions have matched. Catch-all rules must be at the end of a pattern.
+//
+// So with the following patterns, we'll see certain matches:
+//	 router = httptreemux.New()
+//	 router.GET("/:page", pageHandler)
+//	 router.GET("/:year/:month/:post", postHandler)
+//	 router.GET("/:year/:month", archiveHandler)
+//	 router.GET("/images/*path", staticHandler)
+//	 router.GET("/favicon.ico", staticHandler)
+//
+//	 /abc will match /:page
+//	 /2014/05 will match /:year/:month
+//	 /2014/05/really-great-blog-post will match /:year/:month/:post
+//	 /images/CoolImage.gif will match /images/*path
+//	 /images/2014/05/MayImage.jpg will also match /images/*path, with all the text after /images stored in the variable path.
+//	 /favicon.ico will match /favicon.ico
+//
+// # Trailing Slashes
+//
+// The router has special handling for paths with trailing slashes. If a pattern is added to the
+// router with a trailing slash, any matches on that pattern without a trailing slash will be
+// redirected to the version with the slash. If a pattern does not have a trailing slash, matches on
+// that pattern with a trailing slash will be redirected to the version without.
+//
+// The trailing slash flag is only stored once for a pattern. That is, if a pattern is added for a
+// method with a trailing slash, all other methods for that pattern will also be considered to have a
+// trailing slash, regardless of whether or not it is specified for those methods too.
+//
+// This behavior can be turned off by setting TreeMux.RedirectTrailingSlash to false. By
+// default it is set to true. The specifics of the redirect depend on RedirectBehavior.
+//
+// One exception to this rule is catch-all patterns. By default, trailing slash redirection is
+// disabled on catch-all patterns, since the structure of the entire URL and the desired patterns
+// can not be predicted. If trailing slash removal is desired on catch-all patterns, set
+// TreeMux.RemoveCatchAllTrailingSlash to true.
+//
+// 	router = httptreemux.New()
+// 	router.GET("/about", pageHandler)
+// 	router.GET("/posts/", postIndexHandler)
+// 	router.POST("/posts", postFormHandler)
+//
+// 	GET /about will match normally.
+// 	GET /about/ will redirect to /about.
+// 	GET /posts will redirect to /posts/.
+// 	GET /posts/ will match normally.
+// 	POST /posts will redirect to /posts/, because the GET method used a trailing slash.
+func (t *TreeMux) Handle(method, path string, handler HandlerFunc) {
 	if path[0] != '/' {
 		panic(fmt.Sprintf("Path %s must start with slash", path))
 	}
@@ -98,33 +165,40 @@ func (t *TreeMux) Handle(verb, path string, handler HandlerFunc) {
 	if addSlash {
 		node.addSlash = true
 	}
-	node.setHandler(verb, handler)
+	node.setHandler(method, handler)
 }
 
+// Syntactic sugar for Handle("GET", path, handler)
 func (t *TreeMux) GET(path string, handler HandlerFunc) {
 	t.Handle("GET", path, handler)
 }
 
+// Syntactic sugar for Handle("POST", path, handler)
 func (t *TreeMux) POST(path string, handler HandlerFunc) {
 	t.Handle("POST", path, handler)
 }
 
+// Syntactic sugar for Handle("PUT", path, handler)
 func (t *TreeMux) PUT(path string, handler HandlerFunc) {
 	t.Handle("PUT", path, handler)
 }
 
+// Syntactic sugar for Handle("DELETE", path, handler)
 func (t *TreeMux) DELETE(path string, handler HandlerFunc) {
 	t.Handle("DELETE", path, handler)
 }
 
+// Syntactic sugar for Handle("PATCH", path, handler)
 func (t *TreeMux) PATCH(path string, handler HandlerFunc) {
 	t.Handle("PATCH", path, handler)
 }
 
+// Syntactic sugar for Handle("HEAD", path, handler)
 func (t *TreeMux) HEAD(path string, handler HandlerFunc) {
 	t.Handle("HEAD", path, handler)
 }
 
+// Syntactic sugar for Handle("OPTIONS", path, handler)
 func (t *TreeMux) OPTIONS(path string, handler HandlerFunc) {
 	t.Handle("OPTIONS", path, handler)
 }
@@ -241,9 +315,9 @@ func (t *TreeMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	handler(w, r, params)
 }
 
-// MethodNotAllowedHandler is the default handler for
-// TreeMux.MethodNotAllowedHandler. It writes the status code
-// http.StatusMethodNotAllowed, and nothing else.
+// MethodNotAllowedHandler is the default handler for TreeMux.MethodNotAllowedHandler,
+// which is called for patterns that match, but do not have a handler installed for the
+// requested method. It simply writes the status code http.StatusMethodNotAllowed.
 func MethodNotAllowedHandler(w http.ResponseWriter, r *http.Request,
 	methods map[string]HandlerFunc) {
 
